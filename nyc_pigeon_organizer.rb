be799693b3616f778e8c245d244aaef91d39955f
Tony Ho <ho.tonym@gmail.com>
2018-05-13 16:23:32 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,11 +4,11 @@ def nyc_pigeon_organizer(data)
     pigeon_names = {}
 
     data.each do |category, info|
-      info.each do |sub_cat, deet|
-        deet.each do |name|
+      info.each do |key2, value|
+        value.each do |name|
             pigeon_names[name] ||= {}
             pigeon_names[name][category] ||= []
-            pigeon_names[name][category] << sub_cat.to_s
+            pigeon_names[name][category] << key2.to_s
         end
       end
     end
